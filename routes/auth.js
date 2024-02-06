a5b2a9556113b205e49d5fd033149866bf76e542
Madhurgupta12 <om96002@gmail.com>
2024-02-07 04:43:26 +0530

    add Nodemailer

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,17 @@ const jwt=require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const {JWT_SECRET}=require("../keys");
 const requireLogin=require("../middleware/requireLogin");
+
+const nodemailer=require("nodemailer");
+const sendgridTransport=require("nodemailer-sendgrid-transport");
+
+const transporter=nodemailer.createTransport(sendgridTransport({
+  auth:{
+    api_key:"297BD4AB9446B66EDACF84D08218B75E588EEEE6CD56CE8CBBDFD2A16348F8899B1631C8089AD60F9CA73C196EA23F7F"
+
+  }
+}))
+
 router.get("/", (req, res) => {
   res.send("hello");
 });
@@ -31,6 +42,19 @@ router.post("/signup", (req, res) => {
           
             user.save()
             .then((user) => {
+              const mailOptions = {
+                from: 'your_email@example.com',
+                to: user.email,
+                subject: 'Sending email using SendGrid and Node.js',
+                text: 'This is a test email sent using SendGrid and Node.js!'
+              };
+              transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                  console.error('Error occurred: ', error);
+                } else {
+                  console.log('Email sent: ', info.response);
+                }
+              });
          return res.json({ message: "saved successfully" });
             })
             .catch((err) => {
